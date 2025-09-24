13d44cb529d9d1ad2b6750463307e9cef8839112
Dipexiong <peterdick106@gmail.com>
2025-09-24 16:44:03 +0200

    Use volume, improve usage with low vram, improve manager installation

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
 FROM alpine/git:latest AS builder
-WORKDIR /opt
-RUN git clone --depth 1 https://github.com/comfyanonymous/ComfyUI.git
-WORKDIR /opt/ComfyUI
 
 FROM ubuntu:latest
 ENV DEBIAN_FRONTEND=noninteractive
@@ -21,11 +18,8 @@ RUN apt-get install -y python3-pip python3-venv libstdc++-12-dev python3-setupto
     wget https://repo.radeon.com/amdgpu-install/6.4/ubuntu/noble/amdgpu-install_6.4.60400-1_all.deb && \
     apt-get install -y ./amdgpu-install_6.4.60400-1_all.deb
 
-COPY --from=builder /opt/ComfyUI /comfyui
 WORKDIR /comfyui
 
-# Install Comfy-UI Manager
-RUN git clone https://github.com/ltdrdata/ComfyUI-Manager custom_nodes/comfyui-manager
 
 SHELL ["/bin/bash", "-c"]    
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
-VENV_DIR=/venv
+COMFYUI_DIR="/comfyui"
+VENV_DIR=$COMFYUI_DIR/venv
 
-# Setup venv if not already created
-if [ ! -d "$VENV_DIR" ]; then
-    echo "Creating virtual environment..."
-    python3 -m venv "$VENV_DIR"
-    source "$VENV_DIR/bin/activate"
-
-    echo "Installing PyTorch and dependencies..."
-    pip install --upgrade pip
-    pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.2
-    
-    echo "Installing ComfyUI requirements..."
-    pip install -r requirements.txt
-    pip install pyyaml
-
-    echo "Ensuring model directory exists..."
-    MODEL_DIR=models/checkpoints
-    mkdir -p "$MODEL_DIR"
-
-    # Download sample model
-    echo "Downloading SD-1.5 Sample model (v1-5-pruned-emaonly.safetensors)..."
-    wget -nv -O $MODEL_DIR/v1-5-pruned-emaonly.safetensors https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors
+# Clone ComfyUI and Manager, if it does not exist yet
+if [ ! -d "$COMFYUI_DIR/.git" ]; then
+  echo "Cloning ComfyUI into volume..."
+  git clone https://github.com/comfyanonymous/ComfyUI.git "$COMFYUI_DIR"
+  git clone https://github.com/ltdrdata/ComfyUI-Manager "$COMFYUI_DIR"/custom_nodes/comfyui-manager
 else
-    echo "Using existing virtual environment."
-    source "$VENV_DIR/bin/activate"
+  echo "ComfyUI already initialized."
 fi
 
+echo "Creating/Opening virtual environment..."
+python3 -m venv "$VENV_DIR"
+source "$VENV_DIR/bin/activate"
+
+echo "Installing PyTorch and dependencies..."
+pip install --upgrade pip
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.2
+
+echo "Installing ComfyUI requirements..."
+pip install -r requirements.txt
+pip install pyyaml
+
 # Run ComfyUI
-exec "$VENV_DIR/bin/python" main.py
+exec "$VENV_DIR/bin/python" main.py --lowvram
